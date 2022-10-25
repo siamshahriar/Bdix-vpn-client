ab4fd92898dec78da8f512e147f9b51025d208dc
siamshahriar <siamshahriar99@gmail.com>
2022-10-25 16:53:13 +0600

    packages lists loads from leftNavs

--- src/Pages/Shared/PackageDeails/PackageDetails.js
+++ src/Pages/Shared/PackageDeails/PackageDetails.js
@@ -1,9 +1,58 @@
 import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const PackageDetails = () => {
+  const pack = useLoaderData();
+
+  const { _id, name, speed, description, price, realip, connection, img } =
+    pack;
+  const { youtube, facebook, bdix, rawInternet } = speed;
+
   return (
-    <div>
-      <h2>This is Package Details</h2>
+    <div className="w-50 mx-auto">
+      <div className="d-flex justify-content-center mb-3">
+        <h2 className="text-center ctname">Details of the Package</h2>
+        
+      </div>
+      <Card style={{ width: "22rem" }} className="text-center">
+        <Card.Header>{name}</Card.Header>
+        <Card.Img
+          className="img-fluid"
+          variant="top"
+          style={{ height: "" }}
+          src={img}
+        />
+        <Card.Body>
+          <Card.Title className="mb-3">{name}</Card.Title>
+          <Card.Text>
+            <span>RAW - {rawInternet}mbps</span>
+          </Card.Text>
+          <Card.Text>
+            <span>Youtube - {youtube}mbps</span>
+          </Card.Text>
+          <Card.Text>
+            <span>Facebook - {facebook}mbps</span>
+          </Card.Text>
+          <Card.Text>
+            <span>BDIX - {bdix}mbps</span>
+          </Card.Text>
+          <Card.Text>
+            <span>Real IP - {realip}</span>
+          </Card.Text>
+          <Card.Text>
+            <span>Connection - {connection}</span>
+          </Card.Text>
+          <Card.Text>
+            <span>Price - {price} Taka</span>
+          </Card.Text>
+          <Button variant="primary">
+            <Link to={`/packagedetails/${_id}`}>See Details</Link>
+          </Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
     </div>
   );
 };
